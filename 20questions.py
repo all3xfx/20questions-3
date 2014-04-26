88178f22899558324cdd6d6901a428dd044f1cad
Josh <joshua.g.mcquiston@gmail.com>
2014-04-26 12:27:02 -0700

    second commit

--- 20questions.py
+++ 20questions.py
@@ -2,18 +2,48 @@ class Mystery(object):
 	
 	def __init__(self, noun, adjective, verb):
 		self.nouns = noun
-		self.adjective = adjective
-		self.verb = verb
-	def act(self, what):
-		print "bring me a %s %s and %s" % (self.adjective, what, self.verb)
+		self.adjectives = adjective
+		self.verbs = verb
+	def user_guess(self, word, type):
+		if type == "noun":
+			nouns = []
+			nouns = (thing.nouns)
+			if word in nouns:
+				print "Yes, it is a %s" % word
+				exit("you win")
+			else:
+				print "No, it is not a %s" % word
+				#remaining += -1
+				thing.play(remaining)				
+				
+		elif type == "adjective":
+			adjectives = []
+			adjectives = (thing.adjectives)
+			if word in adjectives:
+				print "yes, it is %s" % word
+				exit("good job!)"
+			else:
+				print "No, it is not %s" % word
+				thing.play(remaining)
+			
+	
+	def play(self, remaining):
+		print "you have %s questions, go!" % remaining
+		guess = raw_input(" >")
+		if "is it a" in guess:
+			guess = guess.split(" ")
+			word = guess[-1]
+			thing.user_guess(word, "noun")
+		elif "is it" in guess:
+			guess = guess.split(" ")
+			word = guess[-1]
+			thing.user_guess(word, "adjective")
+		else:
+			print "fail"
+			exit(1)
 
-tree = Mystery("apple", ["green", "shiny"], "eat")
-descriptor = "shiny apple"
-tree.act(descriptor) 
-print tree.nouns
-print "you have 20 questions, go!"
-guess = raw_input(" >")
-if tree.nouns in guess:
-	print "success"
-else:
-	print "fail"
+frog = Mystery("frog","green","jump")
+thing = frog
+remaining = 20
+
+thing.play(20)
